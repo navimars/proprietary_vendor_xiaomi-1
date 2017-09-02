8d4b8d973e6e17fb8eb8be0a412fd0330e67093a
ktommy91 <kemenesi.tamas@gmail.com>
2017-09-02 12:10:31 +0200

    Add missing libqti-iop.so  from oxygen

--- msm8956-common/msm8956-common-vendor.mk
+++ msm8956-common/msm8956-common-vendor.mk
@@ -361,7 +361,9 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/msm8956-common/proprietary/vendor/lib/libqti-wl.so:system/vendor/lib/libqti-wl.so \
     vendor/xiaomi/msm8956-common/proprietary/vendor/lib64/libqti-wl.so:system/vendor/lib64/libqti-wl.so \
     vendor/xiaomi/msm8956-common/proprietary/lib/libcutils.so:system/lib/libcutils.so \
-    vendor/xiaomi/msm8956-common/proprietary/lib64/libcutils.so:system/lib64/libcutils.so 
+    vendor/xiaomi/msm8956-common/proprietary/lib64/libcutils.so:system/lib64/libcutils.so \
+    vendor/xiaomi/msm8956-common/proprietary/vendor/lib/libqti-iop.so:system/vendor/lib/libqti-iop.so \
+    vendor/xiaomi/msm8956-common/proprietary/vendor/lib64/libqti-iop.so:system/vendor/lib64/libqti-iop.so 
 
 
 PRODUCT_PACKAGES += \
